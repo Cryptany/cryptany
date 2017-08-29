e8d13b64f96f0aeb103377901abd6be5dcdbbe50
Eugene Rupakov <eugene.rupakov@aurea.com>
2017-08-29 21:07:19 +0300

    Fixed some typos, applied copyright

--- Core/Settings/DBSettingsProvider.cs
+++ Core/Settings/DBSettingsProvider.cs
@@ -1,7 +1,24 @@
+/*
+   Copyright 2006-2017 Cryptany, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 using System.Data.SqlClient;
-using Avant.Common.Utils;
+using Cryptany.Common.Utils;
 using System;
 using System.Configuration;
+using System.Data;
 
 namespace Cryptany.Common.Settings
 {
@@ -72,7 +89,7 @@ namespace Cryptany.Common.Settings
 		/// </summary>
 		protected override void SaveSettings()
 		{
-			using( SqlConnection con = DBHelper.Connection )
+			using( SqlConnection con = Database.Connection )
 			{
 				ClearTable(con);
 
