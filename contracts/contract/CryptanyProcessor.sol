767680a9f0283c05750fd6b420ee92fb2e0f21ae
Sviatoslav Mikhailov <svyatoslav@Sviatoslavs-MacBook-Pro.local>
2017-09-29 07:29:03 +0300

    initial commit

---
+++ contracts/contract/CryptanyProcessor.sol
@@ -0,0 +1,210 @@
+pragma solidity ^0.4.11;
+
+  /**
+   * @dev Prototype for internal testing
+   */
+contract CryptanyProcessor {
+    
+  /**
+   * @dev Structure for all incoming/outcoming payments
+   */
+    struct PaymentStructure {
+    
+        address fromAddress;
+
+        address toAddress;
+
+        uint paymentSize;
+        
+        string comment;
+    }
+
+
+  /**
+   * @dev Structure for transactions validation
+   */    
+    struct ValidationStructure {
+        
+        address validator;
+        
+        bool result;
+        
+        string comment;
+    }
+    
+    //address of contract owner
+    address  public owner;
+    
+    //all payments agregated by this contract
+    uint public balance;
+    
+    //list of allowed verifiers - might be refactored
+    mapping (address => address) verifiers;
+    
+    //mapping betwen transaction hash and all validators
+    mapping (bytes32 => ValidationStructure) pendingValidations;
+    
+    //mapping betwen transaction hash and payment structure
+    mapping (bytes32 => PaymentStructure) pendingPayments;
+    
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+   /**
+     * @dev Throws if called by any account other than the validators.
+     */
+    modifier onlyValidators() {
+        require(verifiers[msg.sender] == msg.sender);
+        _;
+    }
+    
+   /**
+    * @dev event occurs in each transaction
+    */
+    event sendMoneyEvent(
+        address toPerson, 
+        address fromPerson,
+        uint payment,
+        string comment,
+        bytes32 transactionHash
+        );
+        
+   /**
+    * @dev event occurs in each positive validation
+    */    
+    event transactionApproved(
+        bytes32 transactionHash,
+        address validator,
+        string comment
+        );
+        
+   /**
+    * @dev event occurs in each rejection
+    */    
+    event transactionRejected(
+        bytes32 transactionHash,
+        address validator,
+        string comment
+        );        
+        
+        
+   /**
+    * @dev event occurs when new validatior is added/removed
+    * 
+    */    
+    event changeListOfValidators(
+        string operationType,
+        address validator
+        );
+        
+   /**
+    * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+    * account.
+    */
+    function Ownable() {
+      owner = msg.sender;
+    }    
+
+   /**
+    * @dev Main function to create transaction
+    */
+    function sendMoneyFor(address toPerson, string comment) payable returns (bytes32) {
+        
+        //Seller balance should bigger, than any payment
+        require (toPerson.balance >= msg.value);
+        
+        //var pendingPay = PaymentStructure(msg.sender, toPerson, msg.value, comment);
+        var pendingPay = PaymentStructure(msg.sender, toPerson, msg.value, comment);
+        
+        //hash from all concatinated data
+        var transactionHash = sha3(msg.sender, toPerson, msg.value, block.timestamp);
+
+        pendingPayments[transactionHash]=pendingPay;
+        balance += msg.value;
+        
+        //sendMoneyEvent(toPerson, msg.sender, msg.value, comment, transactionHash);
+        sendMoneyEvent(toPerson, msg.sender, msg.value, comment, transactionHash);
+        
+        //new transaction shoul be added into the pending chain
+        var pendingValidation = ValidationStructure(verifiers[1], false, "Validation required");
+        pendingValidations[transactionHash] = pendingValidation;
+        
+        return transactionHash;
+    }
+    
+   /**
+    * @dev Main function to process transaction
+    */
+    function receiveMoney(bytes32 transactionHash) returns (bool) {
+        PaymentStructure storage p = pendingPayments[transactionHash];
+        
+        //Only money receiver may call this function
+        require (msg.sender == p.toAddress);
+        
+        //only approved transaction may commited
+        require (pendingValidations[transactionHash].validator == 0);
+        
+        //transaction from contract to address
+        p.toAddress.transfer(p.paymentSize);
+        
+        //removing payment element
+        delete pendingPayments[transactionHash];
+        
+        return true;
+        
+    }
+    
+    function addAllowedValidator(address validatorAddress) onlyOwner {
+        verifiers[validatorAddress] = validatorAddress;
+        changeListOfValidators("New validator has been added", validatorAddress);
+    }
+    
+    function removeAllowedValidator(address validatorAddress) onlyOwner {
+        //only existing validator can be removed
+        require (verifiers[validatorAddress]!=0);
+        changeListOfValidators("Validator has been removed", validatorAddress);
+    }
+    
+   /**
+    * @dev function to approve transaction. Should work with many validators.
+    */
+    function provideValidation(bytes32 transaction) onlyValidators {
+        
+        var validation = pendingValidations[transaction];
+        
+        if (validation.validator == msg.sender && validation.result == true){
+            delete pendingValidations[transaction]; 
+            transactionApproved(transaction, validation.validator, validation.comment);
+        }  
+        else
+            transactionRejected(transaction, validation.validator, validation.comment);
+        
+    }
+
+   /**
+    * @dev might be removed later
+    */
+    function parseAddr(string _a) internal returns (address){
+     bytes memory tmp = bytes(_a);
+     uint160 iaddr = 0;
+     uint160 b1;
+     uint160 b2;
+     for (uint i=2; i<2+2*20; i+=2){
+         iaddr *= 256;
+         b1 = uint160(tmp[i]);
+         b2 = uint160(tmp[i+1]);
+         if ((b1 >= 97)&&(b1 <= 102)) b1 -= 87;
+         else if ((b1 >= 48)&&(b1 <= 57)) b1 -= 48;
+         if ((b2 >= 97)&&(b2 <= 102)) b2 -= 87;
+         else if ((b2 >= 48)&&(b2 <= 57)) b2 -= 48;
+         iaddr += (b1*16+b2);
+     }
+     return address(iaddr);
+    }
+
+}
